be3200f38c58401767df1b882bbf4013c18740f6
james bondz <spazma@gmail.com>
2026-05-19 18:05:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 					const manualRepos = [
 						{ name: "video kompressor 10mb", url: "https://github.com/spazma/kompressor-10mb" },
-						{ name: "psp8 menager", url: "https://github.com/spazma/PSP8-menager" },
+						{ name: "paint shop pro 8 menager", url: "https://github.com/spazma/PSP8-menager" },
 						{ name: "foobar 2000 - history panel (SMP)", url: "https://github.com/spazma/foobar2000-history-panel" },
 						{ name: "foobar 2000 - main_player (SMP)", url: "https://github.com/spazma/foobar-SMP-main_player" },
 						{ name: "foobar 2000 - file info (SMP)", url: "https://github.com/spazma/foobar-SMP-file_info" },
